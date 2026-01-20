b73fe6d3d662071449bf7c60d09084847441a82b
Ashen <ashenpramuditha28@gmail.com>
2026-01-20 18:44:18 +0530

    Added base test setup and project structure

---
+++ Base/TestBase.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace CSE2522_Assignment02.Base
+{
+    public class TestBase
+    {
+        protected IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://uitestingplayground.com/");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+                driver.Dispose();
+                driver = null;
+            }
+        }
+    }
+}
