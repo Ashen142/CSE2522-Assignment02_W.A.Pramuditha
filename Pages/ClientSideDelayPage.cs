c057e9781816900787b5e2123844d2cae180aca7
Ashen <ashenpramuditha28@gmail.com>
2026-02-02 16:43:15 +0530

    add clientsidedelay test case

---
+++ Pages/ClientSideDelayPage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace CSE2522_Assignment02.Pages
+{
+    public class ClientSideDelayPage
+    {
+        IWebDriver driver;
+
+        public ClientSideDelayPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        IWebElement button => driver.FindElement(By.Id("ajaxButton"));
+        IWebElement label => driver.FindElement(By.ClassName("bg-success"));
+
+        public void Open()
+        {
+            driver.Navigate().GoToUrl("https://uitestingplayground.com/clientdelay");
+        }
+
+        public void ClickButton()
+        {
+            button.Click();
+        }
+
+        public string GetResult()
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(d =>
+            {
+                try
+                {
+                    var el = d.FindElement(By.ClassName("bg-success"));
+                    return el.Displayed && !string.IsNullOrEmpty(el.Text);
+                }
+                catch
+                {
+                    return false;
+                }
+            });
+
+            return driver.FindElement(By.ClassName("bg-success")).Text;
+        }
+    }
+}
---
+++ Tests/ClientSideDelayTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System.Threading;
+using CSE2522_Assignment02.Base;
+using CSE2522_Assignment02.Pages;
+
+namespace CSE2522_Assignment02.Tests
+{
+    [TestFixture]
+    public class ClientSideDelayTests : TestBase
+    {
+        [Test(Description = "TC_ClientDelay_01_WaitForText")]
+        public void WaitForText()
+        {
+            Assert.That(driver, Is.Not.Null, "WebDriver instance 'driver' must not be null.");
+
+            var page = new ClientSideDelayPage(driver!);
+            page.Open();
+
+            page.ClickButton();
+
+            Thread.Sleep(6000); // simple wait (good enough for assignment)
+
+            Assert.That(page.GetResult(), Is.EqualTo("Data calculated on the client side."));
+        }
+    }
+}
